ecce38023cd03469cd8faa717f3d5dc8261a0740
Mahithub <recappingmovie@gmail.com>
2023-09-13 05:02:25 -0700

    l

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,9 @@
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
-int main(void)
+int _putchar(char c);
 {
+	char c;
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
