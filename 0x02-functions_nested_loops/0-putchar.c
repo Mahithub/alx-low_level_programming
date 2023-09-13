680806a742a7a263e8219d218f5ef3d800171795
Mahithub <recappingmovie@gmail.com>
2023-09-13 04:45:24 -0700

    lol

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _putchar - program that prints putchar
+  * c: for character
+  * Return: always 0
+  */
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
